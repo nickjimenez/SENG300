51a70a82dac735127da6ea9b1bd65b0fa2ff7ac9
nickjimenez <projects.njimenez@gmail.com>
2018-02-15 00:59:30 -0700

    update

--- TestFile.java
+++ TestFile.java
@@ -4,7 +4,6 @@ public class TestFile {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Version Control is AWESOME!");
 		
 		//MORE THINGS
 
